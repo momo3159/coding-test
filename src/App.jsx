6a404c7a010620a3e7eec6306b2a5723ef8aeba4
tkawakita <tommycsth61@gmail.com>
2021-05-27 05:20:31 +0900

    都道府県一覧をRESAS APIから取得する関数を定義した

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import Header from './components/Header';
 
 const App = () => {
+  const baseURL = 'https://opendata.resas-portal.go.jp';
+
+  useEffect(() => {
+    const fetchPrefectures = async () => {
+      const response = await axios.get(`${baseURL}/api/v1/prefectures`, {
+        headers: { 'X-API-KEY': process.env.REACT_APP_API_KEY },
+      });
+
+      return response;
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header title="Title" />
