16d5397e914caf54c1003e65b5a4c6ba4647d4fe
tkawakita <tommycsth61@gmail.com>
2021-05-27 09:47:22 +0900

    取得したデータをもとにグラフを描画するUIを構築した

--- src/components/PrefecturePopulationChart.jsx
+++ src/components/PrefecturePopulationChart.jsx
@@ -1,8 +1,67 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+  Label,
+} from 'recharts';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import styles from './PrefecturePopulationChart.module.css';
 
 const PrefecturePopulationChart = ({ checkedPrefectures }) => {
   const baseURL = 'https://opendata.resas-portal.go.jp';
+  const colors = [
+    '#B71C1C',
+    '#6D4C41',
+    '#4E342E',
+    '#E91E63',
+    '#C2185B',
+    '#C51162',
+    '#9C27B0',
+    '#F44336',
+    '#009688',
+    '#8E24AA',
+    '#8E24AA',
+    '#4CAF50',
+    '#2196F3',
+    '#CDDC39',
+    '#C0CA33',
+    '#42A5F5',
+    '#FFEB3B',
+    '#FDD835',
+    '#0D47A1',
+    '#2962FF',
+    '#3F51B5',
+    '#00BCD4',
+    '#1A237E',
+    '#1DE9B6',
+    '#66BB6A',
+    '#2E7D32',
+    '#00C853',
+    '#8BC34A',
+    '#9CCC65',
+    '#64DD17',
+    '#AEEA00',
+    '#F57F17',
+    '#FFD600',
+    '#FFC107',
+    '#FFB300',
+    '#FF6F00',
+    '#FF9800',
+    '#FB8C00',
+    '#FF6D00',
+    '#FF5722',
+    '#FF7043',
+    '#BF360C',
+    '#795548',
+    '#D50000',
+    '#E65100',
+    '#212121',
+    '#FFAB00',
+  ];
   const [data, setData] = useState([]); // グラフの描画用のデータ
   const [cache, setCache] = useState({}); // APIから取得したことのある都道府県とそのコードをキャッシュ
   const [fetchError, setFetchError] = useState(false);
@@ -61,17 +120,23 @@ const PrefecturePopulationChart = ({ checkedPrefectures }) => {
             for (let i = 0; i < populations.length; i += 1) {
               for (let j = 0; j < populations[i].data.length; j += 1) {
                 if (i === 0) {
-                  copiedData.push({ name: populations[i].data[j].year });
+                  if (populations[i].data[j].year <= new Date().getFullYear()) {
+                    copiedData.push({ name: populations[i].data[j].year });
+                  }
                 }
-                copiedData[j][populations[i].prefName] =
-                  populations[i].data[j].value;
+                if (populations[i].data[j].year <= new Date().getFullYear()) {
+                  copiedData[j][populations[i].prefName] =
+                    populations[i].data[j].value;
+                }
               }
             }
           } else {
             for (let i = 0; i < populations.length; i += 1) {
               for (let j = 0; j < populations[i].data.length; j += 1) {
-                copiedData[j][populations[i].prefName] =
-                  populations[i].data[j].value;
+                if (populations[i].data[j].year <= new Date().getFullYear()) {
+                  copiedData[j][populations[i].prefName] =
+                    populations[i].data[j].value;
+                }
               }
             }
           }
@@ -80,6 +145,88 @@ const PrefecturePopulationChart = ({ checkedPrefectures }) => {
       }
     })();
   }, [data, cache, copiedCheckedPrefectures]);
+  const isPhone = window.innerWidth <= 600;
+
+  return (
+    <>
+      {fetchError && <div>データ取得時にエラーが発生しました</div>}
+      <div
+        style={{
+          width: '100%',
+          height: isPhone ? 500 : 600,
+          margin: '50px auto',
+        }}
+      >
+        <ResponsiveContainer>
+          <LineChart
+            data={data}
+            margin={{
+              left: isPhone ? 5 : 20,
+              top: 50,
+              right: isPhone ? 100 : 200,
+            }}
+          >
+            <XAxis dataKey="name" style={{ fontSize: isPhone ? 10 : 12 }}>
+              <Label
+                value="年度"
+                position="right"
+                offset={20}
+                style={{ fontSize: isPhone ? 10 : 12 }}
+              />
+            </XAxis>
+            <YAxis style={{ fontSize: isPhone ? 10 : 12 }}>
+              <Label
+                value="人口数"
+                position="top"
+                offset={20}
+                style={{ fontSize: isPhone ? 10 : 12 }}
+              />
+            </YAxis>
+
+            {checkedPrefectures.map((p, i) => (
+              <Line
+                type="linear"
+                dataKey={p.prefName}
+                stroke={colors[i]}
+                dot={false}
+                strokeWidth={1.5}
+              />
+            ))}
+            <Legend
+              align="right"
+              verticalAlign="top"
+              layout="vertical"
+              wrapperStyle={{ right: isPhone ? 3 : 15, top: 20 }}
+              content={(props) => {
+                const { payload } = props;
+
+                return (
+                  <ul className={styles['legend-container']}>
+                    {payload.map((entry) => (
+                      <li
+                        className={styles['list-item']}
+                        style={{ color: entry.payload.stroke }}
+                      >
+                        {' '}
+                        <span
+                          style={{
+                            color: 'black',
+                            fontSize: isPhone ? 10 : 12,
+                          }}
+                        >
+                          {entry.value}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                );
+              }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </>
+  );
 };
 
 export default PrefecturePopulationChart;
